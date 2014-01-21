62e1c31505d1613fab3ebb8037f66124e09f6657
Luka Bratos <luka.bratos@gmail.com>
2014-01-21 13:51:43 +0000

    Tests for methods.

--- LBPointKitTests/LBPointKitTests.m
+++ LBPointKitTests/LBPointKitTests.m
@@ -6,9 +6,14 @@
 //  Copyright (c) 2014 Luka. All rights reserved.
 //
 
+#import "LBPointKit.h"
 #import <XCTest/XCTest.h>
 
 @interface LBPointKitTests : XCTestCase
+
+CGPoint CGPointAddition(CGPoint pointA, CGPoint pointB);
+CGPoint CGPointSubtraction(CGPoint pointA, CGPoint pointB);
+CGPoint CGPointMultiplication(float scalar, CGPoint point);
 
 @end
 
@@ -24,6 +29,21 @@
 {
     // Put teardown code here. This method is called after the invocation of each test method in the class.
     [super tearDown];
+}
+
+- (void)testVectorAddition
+{
+    XCTAssertEqual(CGPointAddition(CGPointMake(1, 7), CGPointMake(5, 10)), CGPointMake(6, 17), @"Vector addition failed!");
+}
+
+- (void)testVectorSubstraction
+{
+    XCTAssertEqual(CGPointSubtraction(CGPointMake(1, 7), CGPointMake(5, 10)), CGPointMake(-4, -3), @"Vector substraction failed!");
+}
+
+- (void)testVectorMultiplication
+{
+    XCTAssertEqual(CGPointMultiplication(3, CGPointMake(5, 10)), CGPointMake(15, 30), @"Vector substraction failed!");
 }
 
 @end
